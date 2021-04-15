1e9de7f00b2657b3b78a4da9d1b19386c7045593
Andrew Malchow <wohclams@gmail.com>
2021-04-15 10:30:23 -0500

    Add local turbo.js files

---
+++ static/turbo_load.js
@@ -0,0 +1,7 @@
+/*
+ * Skypack CDN - @hotwired/turbo@7.0.0-beta.4
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/static/turbo.js';
+export {default} from '/static/turbo.js';
